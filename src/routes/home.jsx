0909b4450b4c3cfc288c2d6c86694cd8549728c2
MMv110 <miguelmv@live.com.mx>
2024-06-27 01:12:51 -0600

    Se modificó producto y home

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,47 +1,41 @@
 import Navbar from "./navbar"
+import IMAGES from "../img/images"
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 export default function(){
+
+    const [data, setData ] = useState([])
+    
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(fetchedData => setData(fetchedData))
+            .catch(error => console.log("No andamos josha, error en fetch de home."))
+    }, []);
     return(
-        <>
+        <div>
             <Navbar/>
+            
             <div className="row row-cols-1 row-cols-md-2 g-4">
-            <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
+            
+            {data.map((item) => (
                 <div className="col">
                     <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
+                        <Link to={`/product/${item.id}`}>
+                        <img src={IMAGES[item.title]} className="card-img-top"/>
+                        </Link>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
+                        <Link to={`/product/${item.id}`} className="card-title">
+                            <h5 >{item.title}</h5>
+                        </Link>
+                        <p className="card-text">{item.body}</p>
                     </div>
                 </div>
             </div>
-        </>
+            ))}
+            
+            </div>
+        </div>
     )
 }
