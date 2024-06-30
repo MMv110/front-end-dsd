9a5821e61e3b58261d80b111eccdf81f0c7bd2d1
MMv110 <miguelmv@live.com.mx>
2024-06-29 23:56:46 -0600

    Versión semi-final.

--- src/img/images.jsx
+++ src/img/images.jsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const IMAGES  = {
     image1: new URL('./labial.jpg', import.meta.url).href
 }
 
-=======
-const IMAGES  = {
-    image1: new URL('./labial.jpg', import.meta.url).href
-}
-
->>>>>>> 21bf2b28a18cf0353dda5616c812a24c5ae43b31
 export default IMAGES
